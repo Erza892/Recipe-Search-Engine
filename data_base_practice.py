0139a53c7e8ae33621215984dfb1172911d94d6f
Ayushi Pandey <152494473+Erza892@users.noreply.github.com>
2024-10-03 18:01:50 +0530

    Update data_base_practice.py

--- data_base_practice.py
+++ data_base_practice.py
@@ -15,15 +15,15 @@ cmnd2 = """CREATE TABLE IF NOT EXISTS
 ingredients(recipe_name text primary key, ingredient TEXT, quantity int, units text)"""
 cursor.execute(cmnd2)
 
-cursor.execute("INSERT INTO ingredients values ( 'beef', 1, 'lbs')")
-cursor.execute("INSERT INTO ingredients values ( 'carrots', .5, 'lbs')")
-cursor.execute("INSERT INTO ingredients values ( 'broth', 16, 'oz')")
-cursor.execute("INSERT INTO ingredients values ( 'broccoli',.5,'lbs')")
+cursor.execute("INSERT INTO ingredients values ( 'Paneer', 1, 'lbs')")
+cursor.execute("INSERT INTO ingredients values ( 'Noodles', .5, 'lbs')")
+cursor.execute("INSERT INTO ingredients values ( 'Soup', 16, 'oz')")
+cursor.execute("INSERT INTO ingredients values ( 'Pizza',.5,'lbs')")
 
 
-cursor.execute("INSERT INTO recipes VALUES ('Beef Stew', 'beef, broth, carrots, broccoli')")
+cursor.execute("INSERT INTO recipes VALUES ('Paneer', 'Noodles, Soup, carrots, Pizza')")
 cursor.execute("INSERT INTO recipes VALUES ('Fried Rice', 'Jasmine rice, water, oil, egg, (veggies)')")
-cursor.execute("INSERT INTO recipes VALUES ('Breakfast Burrito', 'tortillas, sausage, egg, cheese, hashbrowns')")
+cursor.execute("INSERT INTO recipes VALUES ('Breakfast Idli', 'Poha, Pan cakes, egg, cheese, Roti')")
 
 cursor.execute("SELECT * FROM ingredients")
 
